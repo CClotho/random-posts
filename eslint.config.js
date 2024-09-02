8e0f4fbecd080ce7e66d5fa35256f39c6f469fce
CClotho <124051470+CClotho@users.noreply.github.com>
2024-09-02 22:41:54 +0800

    removed unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from '@typescript-eslint/eslint-plugin';
 import react from 'eslint-plugin-react';
 
 export default {
