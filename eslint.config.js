ef820106423473ee3d1f9cfef583e85571be604a
CClotho <124051470+CClotho@users.noreply.github.com>
2024-09-02 22:27:25 +0800

    fixed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import react from 'eslint-plugin-react'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import react from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      react,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-        ...react.configs.recommended.rules,
-        ...react.configs['jsx-runtime'].rules,
-      ],
-    },
+export default {
+  root: true,
+  ignorePatterns: ['dist'],
+  extends: [
+    js.configs.recommended,
+    'plugin:@typescript-eslint/recommended', // Instead of spreading configs
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname, // Assuming this is what you meant by `import.meta.dirname`
+    ecmaVersion: 2020,
   },
-)
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: { version: '18.3' },
+  },
+  plugins: ['react-hooks', 'react-refresh', 'react', '@typescript-eslint'],
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
