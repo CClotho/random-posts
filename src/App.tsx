550214624605da7f168f6b163954c2d876b13c3b
CClotho <124051470+CClotho@users.noreply.github.com>
2024-09-02 16:26:30 +0800

    deleted unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+
 import MagazineLayout from './components/MagazineLayout'
 import './styles/app.scss';
-import Post from './components/Posts';
 import PostList from './components/PostList';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
